a6353e8366c5edfcebfdd0147e7eaf723ddcfeba
John David Nolen <jdln@bishop.attlocal.net>
2014-11-19 05:16:45 -0600

    first commit

---
+++ script.sh
@@ -0,0 +1,18 @@
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
+sudo sh -c "echo deb https://get.docker.com/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
+sudo apt-get update
+sudo apt-get -y -q install lxc-docker
+
+#tests for successful install
+
+#assert function for testing
+assert()
+{
+	if hash $1 2>/dev/null; then
+        	echo $1  "installed"
+	else
+        	echo $1 "not installed"
+	fi
+}
+
+assert docker
