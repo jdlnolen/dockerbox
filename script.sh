dc2cc984737fd7de39891ae9ee6112e8261360c1
John David Nolen <jdln@bishop.attlocal.net>
2014-12-06 07:09:02 -0600

    minor tweaks

--- script.sh
+++ script.sh
@@ -1,7 +1,10 @@
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
 sudo sh -c "echo deb https://get.docker.com/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
 sudo apt-get update
+sudo apt-get -y -q install build-essential python-dev libevent-dev python-pip liblzma-dev
 sudo apt-get -y -q install lxc-docker
+
+sudo pip install docker-registry
 
 #tests for successful install
 
